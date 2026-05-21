ac031a05b5ffb36704c118701c913c8aaad3816b
ayshaaktersaima1 <ayshaaktersaima1.com>
2026-05-21 21:42:08 +0600

    updated a bit again

--- src/app/(main)/my-added-cars/update-car/[id]/page.jsx
+++ src/app/(main)/my-added-cars/update-car/[id]/page.jsx
@@ -22,7 +22,7 @@ const UpdateCar = async ({ params }) => {
 
     return (
         <div>
-            <div className="pt-20 pb-11 md:pt-40 md:pb-22 w-[90%] mx-auto">
+            <div className="pt-26 pb-11 md:pt-40 md:pb-22 w-[90%] mx-auto">
                 <h1 className='text-center text-3xl md:text-5xl mb-7 md:mb-10 font-semibold'>Update Car</h1>
 
                 <UpdateCarForm car={car}></UpdateCarForm>
